b6b9f4fcdbe40db7f3bd27068772ce7f1a6de3f4
Charli Russell <Charlirussell2491@gmail.com>
2023-09-22 14:36:52 +0200

    Header and Footer added

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.scss'
+
+function Footer() {
+  return (
+    <>
+      <div className="footer-links">
+        Footer
+      </div>
+    </>
+  )
+}
+
+export default Footer;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import './Header.scss'
+
+function Header() {
+  return (
+    <>
+      <div>
+        Header
+      </div>
+    </>
+  )
+}
+
+export default Header

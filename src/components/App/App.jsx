3f44c1343e66ff862d7b27abde6460cb87f5f7a8
FabienBaraille <fabien.baraille@oclock.school>
2023-09-25 14:07:41 +0200

    start style for connexion form

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,15 @@
 import ExtendedRules from '../Rules/ExtendedRules.jsx';
 import UpButton from './UpButton/UpButton.jsx';
-import Connexion from '../Connexion/Connexion.jsx'
-import SimpleRules from '../Rules/SimpleRules.jsx';
+import Connexion from '../Connexion/Connexion.jsx';
+// import SimpleRules from '../Rules/SimpleRules.jsx';
 
 const App = () => {
   return (
     <>
-      {/* <Connexion /> */}
+      <Connexion />
       {/* <Creation /> */}
       {/* <SimpleRules /> */}
-      <ExtendedRules />
+      {/* <ExtendedRules /> */}
       <UpButton />
     </>
   )

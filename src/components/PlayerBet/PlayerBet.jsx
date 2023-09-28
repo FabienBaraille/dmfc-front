72e90dd5fe32afb0743824a0af0583757985f754
Quentin D <quentin.dumet@gmail.com>
2023-09-28 14:53:32 +0200

    playerBet scss + responsiv

--- src/components/PlayerBet/PlayerBet.jsx
+++ src/components/PlayerBet/PlayerBet.jsx
@@ -7,7 +7,12 @@ const PlayerBet = () => {
     <Wrapper name="player_bet">
     <h2>Pronostique SR : Round 2</h2>
       <div className="player_bet">
-        <PlayerBetMatch />
+        <PlayerBetMatch number="1" />
+        <PlayerBetMatch number="2" />
+        <PlayerBetMatch number="3" />
+        <PlayerBetMatch number="4" />
+        <PlayerBetMatch number="5" />
+        <PlayerBetMatch number="6" />
       </div>
       <button type="submit">Valider mes choix</button>
     </Wrapper>
